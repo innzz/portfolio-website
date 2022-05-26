84bdfa3d36a92d4fb25a1fb848034792349fb710
inzz <inzamampawaskar10@gmail.com>
2022-05-26 16:03:24 +0530

    Revert "Added Project2 in Project section"
    This reverts commit 436c1fcc4003d51ce2a382b35def204d8a745b7b.

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,18 +1,17 @@
 import project1 from '../assets/project1.png';
-import project2 from '../assets/project2.png'
 
 const projectCardData = [
     {
         imgsrc: project1,
         title:"Ecommerce Website",
         text:"Lorem ipsum dolor sit amet.",
-        view:"https://clone-228d2.web.app/"
+        view:"/clone-228d2.web.app/"
     },
     {
-        imgsrc: project2,
-        title:"Tesla-Clone",
+        imgsrc: project1,
+        title:"Ecommerce Website",
         text:"Lorem ipsum dolor sit amet.",
-        view:"https://innzz.github.io/Tesla-Clone/"
+        view:"/clone-228d2.web.app/"
     },
     {
         imgsrc: project1,
