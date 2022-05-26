02a33a3f514f7a3a3e6ebf21949cc642aef30871
inzz <inzamampawaskar10@gmail.com>
2022-05-26 16:12:12 +0530

    Solved some issues in Project 2 section

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,17 +1,18 @@
 import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png'
 
 const projectCardData = [
     {
         imgsrc: project1,
         title:"Ecommerce Website",
         text:"Lorem ipsum dolor sit amet.",
-        view:"/clone-228d2.web.app/"
+        view:"https://clone-228d2.web.app/"
     },
     {
-        imgsrc: project1,
-        title:"Ecommerce Website",
+        imgsrc: project2,
+        title:"Tesla-Clone",
         text:"Lorem ipsum dolor sit amet.",
-        view:"/clone-228d2.web.app/"
+        view:"https://innzz.github.io/Tesla-Clone/"
     },
     {
         imgsrc: project1,
